b70070c654761046da7ea8fab4e0dd848fb6bc7d
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro-2.local>
2023-01-22 18:52:42 -0500

    updated projects description and readme

--- data/projects.ts
+++ data/projects.ts
@@ -2,14 +2,14 @@ const projects_data = [
     {
         name: "Admin Dashboard",
         projectUrl: "https://rinajabu.github.io/admin-dashboard/",
-        description: "A single page application admin dashboard template. Built using React.js with react-router and context API. Material UI was used to create the UI. Built in light/dark mode, various charts, calendar, form with validation, and data grids to display data.",
+        description: "A single page application admin dashboard template. Developed using React.js with react-router and context API. Material UI was used to create the UI. Light/dark mode, various charts, calendar, form with validation, and data grids to display data built in.",
         src: "/projects/admin-dashboard.png",
         alt: "screenshot of Admin Dashboard application"
     },
     {
         name: "Pokemon App",
         projectUrl: "https://distracted-goodall-b55e3c.netlify.app/",
-        description: "A web app in which users can play an immersive Pokémon experience. Used HTML and CSS to create the UI. Used AJAX to retrieve data from Pokemon API and from Weather API. Used jQuery and JavaScript to do DOM and CSS manipulation.",
+        description: "A web app in which users can play an immersive Pokémon experience. HTML and CSS were used to create the UI. Along with jQuery and JavaScript to perform DOM manipulation. Ajax calls to the Pokemon and Weather API's were used to fetch and display data.",
         src: "/projects/pokemon-app.png",
         alt: "screenshot of Pokemon application"
     },
